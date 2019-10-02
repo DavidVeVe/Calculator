3a31809f9f1cc355fa9802fc330f007e02c495e0
Armando Velazquez <davidarmando.gdl@live.com>
2019-10-01 21:50:18 -0500

    Calculator layout done

---
+++ script2.js
@@ -0,0 +1,89 @@
+var tagElement = function (element) {
+    if (element.charAt(0) === '#') {
+        return document.querySelector(element)
+    }
+    else {
+        return document.querySelectorAll(element)
+    }
+}
+
+var screen = tagElement('#screen'),
+    equals = tagElement('#equals'),
+    clear = tagElement('#clear'),
+    numbers = tagElement('.num'),
+    ops = tagElement('.ops'),
+    num = '',
+    backNum = '',
+    result,
+    operator
+
+var startNum = function () {
+    if(result) {
+        num = this.getAttribute('data-num')
+        result = ''
+    }
+    else {
+        num += this.getAttribute('data-num')
+    }
+    screen.innerHTML = num
+}
+
+var moveNumber = function () {
+    backNum = num
+    num = ''
+    operator = this.getAttribute('data-op')
+    equals.setAttribute('data-result', '')
+}
+
+var displayResult = function () {
+
+    backNum = parseFloat(backNum)
+    num = parseFloat(num)
+
+    switch (operator) {
+        case 'plus':
+            result = backNum + num
+        break
+
+        case 'minus':
+            result = backNum - num
+        break
+
+        case 'mult':
+             result = backNum * num
+        break
+
+        case 'divide':
+            result = backNum / num
+        break
+
+        default:
+            result = num
+        }
+
+    screen.innerHTML = result
+    equals.setAttribute('data-result', result)
+
+    backNum = 0
+    num = result
+
+}
+
+function clearAll () {
+    backNum = ''
+    num = ''
+    screen.innerHTML = '0'
+    equals.setAttribute('data-result', result)
+}
+
+for (var i = 0; i < numbers.length; i++) {
+    numbers[i].onclick = startNum
+}
+
+for (var i = 0; i < ops.length; i++) {
+    ops[i].onclick = moveNumber
+}
+
+equals.onclick = displayResult
+
+clear.onclick = clearAll
